1dc2825f773369bba934fa349b6c1688e2aa3335
ohwwh <ohw1237@gmail.com>
2022-09-29 19:57:35 +0900

    10주차

--- hoh/test.js
+++ hoh/test.js
@@ -1,3 +1,15 @@
-console.log(i);
-let i = 10;
+/*function Person(name, gender) {
+	this.name = name;
+	this.gender = gender;
+}
 
+const v = Person("hi", "hi");
+console.log(v);*/
+
+
+let count = function() {
+    console.log('count는 1이다.');
+}
+count();
+
+console.dir(count);
